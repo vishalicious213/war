1e8325a389e2a01565d08a239e2ef646e563bd7b
vishalicious213 <vishalicious213@gmail.com>
2023-05-25 15:31:07 -0700

    getting deck_id from deckofcards and saving it to deckID

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const newDeckBtn = document.getElementById("new-btn")
+let deckID = ""
 
 // ⬇️ EVENT LISTENERS ⬇️
 
@@ -9,5 +10,9 @@ newDeckBtn.addEventListener("click", getNewDeck)
 function getNewDeck() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            deckID = data.deck_id
+            console.log(deckID)
+        })
 }
