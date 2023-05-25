509e81805fb2a724a4c43b32b8463f6dc4504c4c
vishalicious213 <vishalicious213@gmail.com>
2023-05-25 15:35:15 -0700

    added draw button. took control of it in JS. added event listener and drafted event handler for it (getNewCards())

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const newDeckBtn = document.getElementById("new-btn")
+const drawBtn = document.getElementById("draw-btn")
 let deckID = ""
 
 // ⬇️ EVENT LISTENERS ⬇️
 
 newDeckBtn.addEventListener("click", getNewDeck)
+
+drawBtn.addEventListener("click", getNewCards)
 
 // ⬇️ EVENT HANDLERS ⬇️
 
@@ -15,4 +18,8 @@ function getNewDeck() {
             deckID = data.deck_id
             console.log(deckID)
         })
+}
+
+function getNewCards() {
+    console.log("GET")
 }
